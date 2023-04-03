1f4437a0220091ce9c7b658c27ea7b75b019fde1
dbs271 <djaudwls7873@gmail.com>
2023-04-03 22:07:22 +0900

    feat/문제풀이추가

--- programmers/day03/최빈값구하기.js
+++ programmers/day03/최빈값구하기.js
@@ -42,3 +42,13 @@
 
 ※ 공지 - 2022년 10월 17일 제한 사항 및 테스트케이스가 수정되었습니다.
 */
+
+function solution(array) {
+  let obj = {};
+  let arr = [];
+  for (let i of array) obj[i] ? ++obj[i] : (obj[i] = 1);
+  for (let i in obj) arr.push([i, obj[i]]);
+  arr.sort((a, b) => b[1] - a[1]);
+  if (arr.length > 1 && arr[0][1] === arr[1][1]) return -1;
+  return Number(arr[0][0]);
+}
