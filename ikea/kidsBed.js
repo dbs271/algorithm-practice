8d0cbe96af6b176fe4d98209e9ea812478c18d64
dbs271 <djaudwls7873@gmail.com>
2023-04-06 01:18:06 +0900

    feat/ ikea loft 침대 예시완성

--- ikea/kidsBed.js
+++ ikea/kidsBed.js
@@ -1,65 +1,100 @@
-const bedName = ["스모스타드", "스베르타"];
-const bedSize = "90x200cm";
-const loftBed = "로프트 침대";
-const loftBedFrame = "로프트 침대 프레임";
-const bedColors = [
-  "화이트 그레이",
-  "화이트 그린",
-  "화이트 자작나무",
-  "화이트 칠판표면",
-  "화이트 페일 터콰이즈",
-  "화이트 페일 핑크",
-  "화이트 화이틑 프레임",
-  "화이트 화이트",
-];
-const bed5Colors = [
-  "화이트 그레이",
-  "화이트 그린",
-  "화이트 페일 터콰이즈",
-  "화이트 페일 핑크",
-  "화이트 화이트",
-];
+// 침대 브랜드명
+const bedName = {
+  smostad: "스모스타드",
+  svarta: "스베르타",
+};
+// 침대 사이즈
+const bedSize = { average: "90x200cm" };
+// 침대 종류
+const bedType = {
+  loftBed: "로프트침대",
+  loftBedFrame: "로프트침대프레임",
+};
+// 침대 색상
+const bedColors = {
+  whiteGray: "화이트 그레이",
+  whiteGreen: "화이트 그린",
+  whiteWood: "화이트 자작나무",
+  whiteBoard: "화이트 칠판표면",
+  whitePaleTurquoise: "화이트 페일 터콰이즈",
+  whitePalePink: "화이트 페일 핑크",
+  whiteFrame: "화이트 화이트 프레임",
+  white: "화이트 화이트",
+  silver: "실버",
+};
 
 kidBeds = {
+  // 로프트침대
   loftBed: {
     SMASTAD_1: {
       id: 1,
-      name: bedName[0],
-      color: bedColors[5],
+      name: bedName.smostad,
+      classiFication: bedType.loftBed,
+      color: bedColors.whitePalePink,
       composition: "책상+선반2",
-      size: bedSize,
+      size: bedSize.average,
       price: "743,000",
-      colorPick: bed5Colors,
+      colorPick: [
+        bedColors.whiteGray,
+        bedColors.whiteGreen,
+        bedColors.whitePaleTurquoise,
+        bedColors.whitePalePink,
+        bedColors.white,
+      ],
     },
     SMASTAD_2: {
       id: 2,
-      name: bedName[0],
-      color: bedColors[5],
-      classiFication: loftBed,
+      name: bedName.smostad,
+      classiFication: bedType.loftBed,
+      color: bedColors.whitePalePink,
       composition: "책상+서랍4",
-      size: bedSize,
+      size: bedSize.average,
       price: "718,000",
-      colorPick: bedColors,
+      colorPick: [
+        bedColors.whiteGray,
+        bedColors.whiteGreen,
+        bedColors.whiteWood,
+        bedColors.whiteBoard,
+        bedColors.whitePaleTurquoise,
+        bedColors.whitePalePink,
+        bedColors.whiteFrame,
+        bedColors.white,
+      ],
     },
     SMOSTAD_3: {
       id: 3,
-      name: bedName[0],
-      color: bedColors[7],
-      classiFication: loftBed,
+      name: bedName.smostad,
+      classiFication: bedType.loftBed,
+      color: bedColors.white,
       composition: "책상+서랍3",
-      size: bedSize,
+      size: bedSize.average,
       price: "696,000",
-      colorPick: bedColors,
+      colorPick: [
+        bedColors.whiteGray,
+        bedColors.whiteGreen,
+        bedColors.whiteWood,
+        bedColors.whiteBoard,
+        bedColors.whitePaleTurquoise,
+        bedColors.whitePalePink,
+        bedColors.whiteFrame,
+        bedColors.white,
+      ],
     },
     SVARTA_1: {
       id: 4,
-      name: bedName[1],
-      color: "실버",
+      name: bedName.svarta,
+      classsiFication: bedType.loftBedFrame,
+      color: bedColors.silver,
+      size: bedSize.average,
+      price: "199,000",
     },
   },
+  // 싱글침대
   singleBed: {},
   // 길이조절침대
   adjustableBed: {},
   bunkerBed: {},
   bedAccessory: {},
 };
+
+console.log(kidBeds.loftBed.SMASTAD_1.name);
