9af806894ed96b2d10282a434e01ced6c3037d1a
윤국현 <yungughyeon@yungughyeon-ui-MacBookAir.local>
2023-05-03 22:12:18 +0900

    feat/programmers day11

---
+++ basic/FetchAndAxios/index.js
@@ -0,0 +1,21 @@
+/*
+챗GPT에 Fetch와 Axios 문제 만들어달라고 했습니다! 물론 리액트가 나온 뒤에 잘 쓰이지 않는 방법이지만 고전적인 방법이니 연습하고 싶어서 가져왔습니다!
+여기 JavaScript의 fetch와 axios를 연습하기 위한 문제들을 제공합니다.
+
+문제 1: Fetch
+fetch API를 사용하여 다음 URL로 GET 요청을 보내는 JavaScript 함수를 작성하세요: "https://jsonplaceholder.typicode.com/posts/1". 이 함수는 JSON 응답에서 "title" 속성을 추출하여 반환해야 합니다.
+
+문제 2: Axios
+axios 라이브러리를 사용하여 다음 URL로 POST 요청을 보내는 JavaScript 함수를 작성하세요: "https://jsonplaceholder.typicode.com/posts". 이 함수는 "title"과 "body"라는 속성을 포함한 JSON 객체를 요청 본문으로 보내야 합니다. 함수는 서버로부터의 JSON 응답을 콘솔에 출력해야 합니다.
+
+문제 3: Fetch
+fetch API를 사용하여 다음 URL로 GET 요청을 보내는 JavaScript 함수를 작성하세요: "https://api.coindesk.com/v1/bpi/currentprice/BTC.json". 이 함수는 JSON 응답에서 현재 비트코인의 환율을 나타내는 "rate" 속성을 추출하여 웹 페이지에 표시해야 합니다.
+
+문제 4: Axios
+axios 라이브러리를 사용하여 다음 URL로 GET 요청을 보내는 JavaScript 함수를 작성하세요: "https://dog.ceo/api/breeds/list/all". 이 함수는 JSON 응답에서 모든 개 품종의 이름을 추출하고 웹 페이지에 표시해야 합니다.
+
+문제 5: Fetch와 Axios
+먼저 fetch API를 사용하여 다음 URL로 GET 요청을 보냅니다: "https://jsonplaceholder.typicode.com/posts". 그런 다음 axios를 사용하여 응답에서 반환된 게시물 중 하나의 제목과 내용을 업데이트하는 PUT 요청을 보냅니다. 마지막으로, 업데이트된 게시물을 삭제하기 위해 다시 fetch를 사용하여 DELETE 요청을 보냅니다.
+
+참고: 외부 API에 요청을 보내는 문제의 경우, 해당 API에 액세스하는 권한이 있는지 확인하고, 해당 API의 이용 약관을 준수해야 합니다.
+*/
